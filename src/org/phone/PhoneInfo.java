df64f5aaa69248e80f79d66bca2bcf26a553e7c2
gowtham <its4gautam@gmail.com>
2022-07-30 16:00:18 +0530

    modification

---
+++ src/org/phone/PhoneInfo.java
@@ -0,0 +1,34 @@
+package org.phone;
+
+public class PhoneInfo {
+	private void phoneName() {
+		//giving phone name
+		System.out.println("Phone Name: Nokia 1100");
+	}
+    private void phoneMieiNumber() {
+    	//giving phonemieinumber;
+    	System.out.println("MieiNumber: 7876537Hd9K");
+    }
+    private void camera() {
+    	//giving Camera pixel
+    	System.out.println("Camera Pixel: 128MP");
+    }
+    private void storage() {
+    	//giving memory storage details 
+    	System.out.println("Internal storage: 1TB");
+    }
+    private void osName() {
+    	// giving phone os version details
+    	System.out.println("Phone Os version: Android9.1");
+    }
+    public static void main(String[] args) {
+		PhoneInfo p= new PhoneInfo();
+		p.osName();
+		p.phoneMieiNumber();
+		p.phoneName();
+		p.storage();
+		p.camera();
+	}
+}
+
+
